936ee4639cf5ab6a2fc375ecd32c30881881a36c
Telofy <drescher@ferret-go.com>
2012-11-26 14:12:55 +0100

    lxml dependency

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-
+# -*- encoding: utf-8 -*-
 try:
     from setuptools import setup
 except ImportError:
     from distutils import setup
-    
+
 setup(
-      name='python-sitemap',
-      version='0.1.0',
-      description='Python library for parsing and generating sitemaps',
-      author='Andrei Savu',
-      author_email='asavu@apache.org',
-      url='http://github.com/andreisavu/python-sitemap/tree/master',
-      packages=['sitemap'],
-      package_data={'sitemap' : ['schemas/*.xsd']}
+    name='python-sitemap',
+    version='0.1.0',
+    description='Python library for parsing and generating sitemaps',
+    author='Andrei Savu',
+    author_email='asavu@apache.org',
+    url='http://github.com/andreisavu/python-sitemap/tree/master',
+    packages=['sitemap'],
+    package_data={'sitemap' : ['schemas/*.xsd']},
+    install_requires=[
+        'lxml',
+    ]
 )
